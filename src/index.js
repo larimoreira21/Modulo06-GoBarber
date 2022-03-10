257ba2f90eea41fee02e353ac282ac869bcf5841
Larissa Moreira <larissamoreira@MacBook-Pro-de-Larissa.local>
2022-03-10 19:00:31 -0300

    My message

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import './config/ReactotronConfig';
+import Routes from './routes';
+
+
+Routes
 
 export default function App() {
   return (
---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import { createAppContainer, createStackNavigator } from 'react-navigation';
+import Main from './pages/Main';
+import User from './pages/User';
+
+const Routes = createAppContainer(
+  createStackNavigator({
+    Main,
+    User
+  })
+);
+
+export default Routes;
